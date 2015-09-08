2d137c7147fb5c32c6ff099b255dd03d326dde8d
quanpham87 <quanphamminh@gmail.com>
2015-09-07 23:08:19 -0500

    q44 complete

--- python/e4.py
+++ python/e4.py
@@ -28,5 +28,24 @@ reverse(-123)
 # add 2 binary # that is stored as array
 def addBin(n1, n2):
     print("--Q44--")
+    carry = 0
+    out = ""
+    while (len(n1) > 0 or len(n2) > 0):
+        b1 = n1.pop(-1)
+        b2 = n2.pop(-1)
+        if b1 != b2:
+            if carry == 0:
+                out = "1" + out
+            else:
+                out = "0" + out
+        else:
+            if carry == 0:
+                out = "0" + out
+            else:
+                out = "1" + out
+                carry = 0
+            if b1 == 1:
+                carry = 1
+    print(out)
     
 addBin([0, 0, 1], [1, 0, 1])
