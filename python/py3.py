b2974bcf187aa169a0cad9d77984c96e0027b3d6
Quan Pham <quanphamminh@gmail.com>
2015-09-09 17:16:21 -0500

    update with simplified version

--- python/py3.py
+++ python/py3.py
@@ -1,5 +1,11 @@
-def productFib(prod):
+def productFibV2(prod):
     f = [0, 1]
     while (f[-1] * f[-2] < prod):
         f.append(f[-1] + f[-2])
     return [f[-2], f[-1], f[-2] * f[-1] == prod]
+    
+def productFib(prod):
+    a, b = 0, 1
+    while (a * b < prod):
+        a, b = b, a + b
+    return [a, b, a * b == prod]
