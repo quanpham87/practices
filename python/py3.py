16d84b8e00bf7707040dad2f17d5219b7e37abb0
Quan Pham <quanphamminh@gmail.com>
2015-09-09 17:30:56 -0500

    add practice #4: find the perfect power

--- python/py3.py
+++ python/py3.py
@@ -9,3 +9,16 @@ def productFib(prod):
     while (a * b < prod):
         a, b = b, a + b
     return [a, b, a * b == prod]
+
+import math
+
+def isPP(n):
+    #your code here
+    i = 2
+    while (i <= math.sqrt(n)):        
+        count = 1
+        while (i**count < n):
+            count += 1
+        if (i**count == n):
+            return [i, count]
+        i += 1
